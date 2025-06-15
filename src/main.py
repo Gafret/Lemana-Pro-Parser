9bd085a42c2df73edd17e2ca680da5057afda04b
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-15 09:12:38 +0300

    feat: add logger

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ import os
 import requests
 from requests.exceptions import InvalidJSONError, RequestException, Timeout, HTTPError
 from dotenv import load_dotenv
+import structlog
+
+log = structlog.get_logger()
 
 load_dotenv()
 
@@ -17,6 +20,7 @@ HEADERS = {
     "Mobile-Version": os.getenv("MOBILE_VERSION"),
     "Mobile-Version-Os": os.getenv("MOBILE_VERSION_OS"),
     "Mobile-Build": os.getenv("MOBILE_BUILD"),
+
     "Host": "mobile.api-lmn.ru",
     "Ab-Test-Option": "opt1",
     "User-Agent": "ktor-client",
@@ -38,9 +42,6 @@ HEADERS = {
 # TODO: refactor exception handling
 # TODO: add some fresh docs
 # TODO: allow to choose from which point to start
-# TODO: add CLI interface
-# TODO: add headers setup with dot env config or similar
-# TODO: probably retry then got zero items
 
 class LemanaProItemParser:
     search_url = "https://mobile.api-lmn.ru/mobile/v2/search"
@@ -62,7 +63,7 @@ class LemanaProItemParser:
             *,
             only_available: bool = True,
             show_services: bool = False,
-            show_facets: bool = False
+            show_facets: bool = False,
     ):
         search_body = self._create_search_body(
             catalogue_item=catalogue_item,
@@ -82,28 +83,26 @@ class LemanaProItemParser:
 
                 try:
                     response = self.session.post(url=self.search_url, json=search_body)
-                    print(response.status_code)
                     headers = response.headers
                     body = response.json()
                 except (InvalidJSONError, Timeout, HTTPError, RequestException) as err:
                     if isinstance(err, InvalidJSONError):
-                        print(response.text)
-                        print("Something wrong with response json", err)
+                        log.error("Something wrong with response json", err, response.status_code)
+
                     elif isinstance(err, Timeout):
-                        print(response.text)
-                        print("Too much time has passed", err)
-                        print("Retry in 20 sec")
+                        log.warn("Too much time has passed, retrying", err, response.status_code)
                         time.sleep(20)
+                        continue
+
                     elif isinstance(err, HTTPError):
-                        print(response.text)
-                        print("Wrong response status", err)
+                        log.error("Wrong response status", err, response.status_code)
+
                     elif isinstance(err, RequestException):
-                        print(response.text)
-                        print("Something with request", err)
+                        log.error("Something with request", err, response.status_code)
+
                     break
                 except Exception as err:
-                    print(response.text)
-                    print("Unknown issue", err)
+                    log.error("Unknown issue", err, response.status_code)
                     break
 
                 items = body.get("items", [])
@@ -118,7 +117,7 @@ class LemanaProItemParser:
                 if offset > total_item_count:
                     break
 
-                print(
+                log.info(
                     f"-STATS-\n"
                     f"TOTAL_ITEMS: {total_item_count}\n"
                     f"ITEMS_IN_RESPONSE: {item_array_length}\n"
@@ -154,7 +153,7 @@ class LemanaProItemParser:
                     data_writer.writerow([id, name, brand, regular_price, discount_price])
 
                 if rate_limit_remaining < 5000:
-                    print("WE NEED TO WAIT TO RESET LIMITER")
+                    log.info("WE NEED TO WAIT TO RESET LIMITER")
                     time.sleep(secs_until_reset + 10)
 
                 page_counter += 1
@@ -185,6 +184,9 @@ class LemanaProItemParser:
         return search_body
 
     def _create_checkpoint(self):
+        pass
+
+    def _load_checkpoint(self):
         pass
 
 
